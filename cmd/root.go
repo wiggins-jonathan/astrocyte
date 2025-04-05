4117977a07f4edce3a2e4dcc3ca37c659eb87089
Jonathan Wiggins <jonathan@wiggins.tech>
2025-04-05 14:38:16 -0700

    add --config, -c flag

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,8 @@ package cmd
 import (
 	"fmt"
 	"log/slog"
+	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -24,12 +26,22 @@ func NewRootCmd(v *viper.Viper) *cobra.Command {
 	rootCmd.PersistentFlags().String("log-level", "info", "set log level")
 	v.BindPFlag("log-level", rootCmd.PersistentFlags().Lookup("log-level"))
 
+	// even though we don't use viper/cobra to parse for the config file path
+	// we still want to bind the flag to get correct usage().
+	rootCmd.PersistentFlags().StringP("config", "c", "", "path to the config file")
+	v.BindPFlag("log-level", rootCmd.PersistentFlags().Lookup("log-level"))
+
 	return rootCmd
 }
 
 // Entrypoint for the CLI. Bootstraps config, logger, & registers subcommands
 func Execute() error {
-	cfg, err := NewConfig()
+	configPath, err := getConfigFlag()
+	if err != nil {
+		return err
+	}
+
+	cfg, err := NewConfig(WithConfigPath(configPath))
 	if err != nil {
 		return fmt.Errorf("Error initializing config: %w", err)
 	}
@@ -49,4 +61,39 @@ func Execute() error {
 	}
 
 	return rootCmd.Execute()
+}
+
+// getConfigFlag() parses CLI args in case the user wants to change the config
+// file path. This must be done at the very beginning of startup to avoid a
+// chicken/egg scenario. This spaghetti is why people use cobra/viper
+func getConfigFlag() (string, error) {
+	for i, arg := range os.Args {
+		if arg == "--config" || arg == "-c" {
+			if i+1 >= len(os.Args) { // user passed flag but no argument
+				return "", fmt.Errorf("Error: flag needs an argument: %s", arg)
+			}
+
+			return os.Args[i+1], nil
+		}
+
+		// deal with an equals sign
+		if strings.HasPrefix(arg, "--config=") || strings.HasPrefix(arg, "-c=") {
+			filepath := strings.SplitN(arg, "=", 2)[1]
+			filepath = strings.TrimSpace(filepath)
+
+			if filepath == "" {
+				return "", fmt.Errorf("Error: flag needs an argument: %s", arg)
+			}
+
+			return filepath, nil
+		}
+	}
+
+	// deal with environment variable
+	filepath, exists := os.LookupEnv("ASTROCYTE_CONFIG")
+	if !exists {
+		return "", nil // no flag or environment variable passed
+	}
+
+	return filepath, nil
 }
